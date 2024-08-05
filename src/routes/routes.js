403b76933f7676812e34e1a589a279a566e4bdd2
Felipe <fvhmanhaes@gmail.com>
2024-08-05 01:27:51 -0300

    Finish a presentable version of docs with Swagger

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const usuariosRoutes = require("./usuarios.routes");
-const locaisRoutes = require("./locais.routes");
+const localRoutes = require("./local.routes");
 const LoginController = require('../controllers/LoginController')
 
 const routes = new Router();
@@ -12,8 +12,42 @@ const swaggerDocument = require('./doc.swagger.json');
 routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes.use("/usuario", usuariosRoutes);
-routes.use("/local", locaisRoutes);
-routes.post('/login', LoginController.login)
+routes.use("/local", localRoutes);
+routes.post('/login', 
+   /* 
+     #swagger.tags = ['Login'],
+     #swagger.description = 'Endpoint para login',
+     #swagger.parameters['login'] = {
+         in: 'body',
+         description: 'Credenciais de login',
+         required: true,
+         schema: {
+             $email: "teste@gmail.com",
+             $senha: "teste123"
+         }
+     }
+     #swagger.responses[200] = {
+         description: 'Login bem sucedido',
+         schema: {
+             token: "token_jwt",
+             name: "Nome do Usuário"
+         }
+     }
+     #swagger.responses[400] = {
+         description: 'Email e senha são obrigatórios',
+         schema: { message: "Email and password are required" }
+     }
+     #swagger.responses[404] = {
+         description: 'Conta não encontrada',
+         schema: { message: "Account not found" }
+     }
+     #swagger.responses[500] = {
+         description: 'Erro ao fazer login',
+         schema: { message: "Error logging in" }
+     }
+   */
+   LoginController.login
+ );
 
 
 module.exports = routes;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,10 +4,177 @@ const checkUserHasLocal = require('../middlewares/checkUserHasLocal');
 
 const usuariosRoutes = new Router();
 
-usuariosRoutes.post('/', UsuarioController.create);
-usuariosRoutes.get('/', UsuarioController.searchAll);
-usuariosRoutes.delete('/:id', checkUserHasLocal, UsuarioController.delete); 
-usuariosRoutes.put('/:id', UsuarioController.update);
-usuariosRoutes.get('/:id', UsuarioController.searchOne);
+usuariosRoutes.post('/',
+  /* 
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para criar um usuário',
+    #swagger.parameters['criarUsuario'] = {
+        in: 'body',
+        description: 'Dados do usuário',
+        required: true,
+        schema: {
+            $nome: "Usuario Teste",
+            $email: "teste@gmail.com",
+            $senha: "teste123",
+            $cpf: "12345678900",
+            dataNascimento: "2000-01-01",
+            endereco: "Rua Teste, 123",
+            sexo: "M"
+        }
+    }
+    #swagger.responses[201] = {
+        description: 'Usuário criado com sucesso',
+        schema: {
+            id: 1,
+            nome: "Usuario Teste",
+            email: "teste@gmail.com",
+            cpf: "12345678900",
+            dataNascimento: "2000-01-01",
+            endereco: "Rua Teste, 123",
+            sexo: "M"
+        }
+    }
+    #swagger.responses[400] = {
+        description: 'Erro na validação dos dados',
+        schema: { errors: [{ msg: "Mensagem de erro", param: "campo" }] }
+    }
+    #swagger.responses[409] = {
+        description: 'Conta já existe',
+        schema: { message: "Account already exists" }
+    }
+    #swagger.responses[500] = {
+        description: 'Erro ao criar usuário',
+        schema: { mensagem: "Unable to create user" }
+    }
+  */
+  UsuarioController.create
+);
+
+usuariosRoutes.get('/',
+  /* 
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para buscar todos os usuários',
+    #swagger.parameters['nome'] = { in: 'query', description: 'Nome do usuário', required: false }
+    #swagger.parameters['cpf'] = { in: 'query', description: 'CPF do usuário', required: false }
+    #swagger.parameters['email'] = { in: 'query', description: 'Email do usuário', required: false }
+    #swagger.parameters['dataNascimento'] = { in: 'query', description: 'Data de nascimento do usuário', required: false }
+    #swagger.parameters['endereco'] = { in: 'query', description: 'Endereço do usuário', required: false }
+    #swagger.parameters['sexo'] = { in: 'query', description: 'Sexo do usuário', required: false }
+    #swagger.responses[200] = {
+        description: 'Usuários buscados com sucesso',
+        schema: [{ 
+            id: 1,
+            nome: "Usuario Teste",
+            email: "teste@gmail.com",
+            cpf: "12345678900",
+            dataNascimento: "2000-01-01",
+            endereco: "Rua Teste, 123",
+            sexo: "M"
+        }]
+    }
+    #swagger.responses[500] = {
+        description: 'Erro ao buscar usuários',
+        schema: { mensagem: "Unable to search for users" }
+    }
+  */
+  UsuarioController.searchAll
+);
+
+usuariosRoutes.put('/:id',
+  /* 
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para atualizar um usuário',
+    #swagger.parameters['id'] = { in: 'path', description: 'ID do usuário', required: true }
+    #swagger.parameters['atualizarUsuario'] = {
+        in: 'body',
+        description: 'Dados do usuário para atualizar',
+        required: true,
+        schema: {
+            nome: "Usuario Atualizado",
+            email: "atualizado@gmail.com",
+            senha: "atualizado123",
+            cpf: "12345678901",
+            dataNascimento: "2000-02-02",
+            endereco: "Rua Atualizada, 123",
+            sexo: "F"
+        }
+    }
+    #swagger.responses[200] = {
+        description: 'Usuário atualizado com sucesso',
+        schema: {
+            id: 1,
+            nome: "Usuario Atualizado",
+            email: "atualizado@gmail.com",
+            cpf: "12345678901",
+            dataNascimento: "2000-02-02",
+            endereco: "Rua Atualizada, 123",
+            sexo: "F"
+        }
+    }
+    #swagger.responses[400] = {
+        description: 'Erro na validação dos dados',
+        schema: { errors: [{ msg: "Mensagem de erro", param: "campo" }] }
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado',
+        schema: { mensagem: "User id not found" }
+    }
+    #swagger.responses[500] = {
+        description: 'Erro ao atualizar usuário',
+        schema: { mensagem: "Unable to update user" }
+    }
+  */
+  UsuarioController.update
+);
+
+usuariosRoutes.delete('/:id', 
+  checkUserHasLocal,
+  /* 
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para deletar um usuário',
+    #swagger.parameters['id'] = { in: 'path', description: 'ID do usuário', required: true }
+    #swagger.responses[204] = {
+        description: 'Usuário deletado com sucesso'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado',
+        schema: { mensagem: "User with the given id was not found" }
+    }
+    #swagger.responses[500] = {
+        description: 'Erro ao deletar usuário',
+        schema: { mensagem: "Unable to search for user" }
+    }
+  */
+  UsuarioController.delete
+);
+
+usuariosRoutes.get('/:id',
+  /* 
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para buscar um usuário pelo ID',
+    #swagger.parameters['id'] = { in: 'path', description: 'ID do usuário', required: true }
+    #swagger.responses[200] = {
+        description: 'Usuário buscado com sucesso',
+        schema: {
+            id: 1,
+            nome: "Usuario Teste",
+            email: "teste@gmail.com",
+            cpf: "12345678900",
+            dataNascimento: "2000-01-01",
+            endereco: "Rua Teste, 123",
+            sexo: "M"
+        }
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado',
+        schema: { mensagem: "Unable to find an user with the given id" }
+    }
+    #swagger.responses[500] = {
+        description: 'Erro ao buscar usuário',
+        schema: { mensagem: "Unable to search for user" }
+    }
+  */
+  UsuarioController.searchOne
+);
 
 module.exports = usuariosRoutes;
